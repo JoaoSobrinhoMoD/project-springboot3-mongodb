7200e739b17b71b8ace9cfabee4a5ec825619d39
JoaoSobrinhoMoD <joaosobrinho9000@gmail.com>
2024-09-18 16:37:30 -0300

    Obtendo um usuário por id

--- src/main/java/com/joaosobrinho/projectmongo/services/UserService.java
+++ src/main/java/com/joaosobrinho/projectmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.joaosobrinho.projectmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joaosobrinho.projectmongo.domain.User;
 import com.joaosobrinho.projectmongo.repository.UserRepository;
+import com.joaosobrinho.projectmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
