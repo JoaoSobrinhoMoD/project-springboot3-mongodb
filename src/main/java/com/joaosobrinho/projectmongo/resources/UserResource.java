7ae5d065d5b9de102b84c6ac5b1836e02fb7d9e1
JoaoSobrinhoMoD <joaosobrinho9000@gmail.com>
2024-09-20 15:04:34 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/joaosobrinho/projectmongo/resources/UserResource.java
+++ src/main/java/com/joaosobrinho/projectmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.joaosobrinho.projectmongo.domain.Post;
 import com.joaosobrinho.projectmongo.domain.User;
 import com.joaosobrinho.projectmongo.dto.UserDTO;
 import com.joaosobrinho.projectmongo.services.UserService;
@@ -33,7 +34,7 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-
+	
 	@GetMapping("/{id}")
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping("/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
